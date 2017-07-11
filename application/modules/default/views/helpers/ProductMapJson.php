7591906cea98240b1007c5354dfd135c0a1b356d
Nhan Nguyen <nguyennhan09cntt@gmail.com>
2017-07-11 22:27:26 +0700

    Fixed map

--- application/modules/default/views/helpers/ProductMapJson.php
+++ application/modules/default/views/helpers/ProductMapJson.php
@@ -14,64 +14,63 @@ class View_Helper_ProductMapJson extends Zend_View_Helper_Abstract
         $dataDistrict = Model_District::getInstance()->getAll();
         $categoryData = Model_ProductCategory::getInstance()->getAll();
         foreach ($productList as $product) {
-            try {
-                $link = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=' . 330;
-                $path = !$product[DbTable_Product::COL_PRODUCT_FLAG_UPLOAD_IMAGE] ? '' : 'http://' . Application_Constant_Global::DOMAIN . '/upload/';
-                $image = $product[DbTable_Product::COL_PRODUCT_THUMB_NAIL];
-                if (!$path && !$image) {
-                    $image = 'no-image.png';
-                } else if ($path) {
-                    if ($image) {
-                        $position = strpos($image, '.');
-                        if ($product[DbTable_Product::COL_PRODUCT_FLAG_CROP] == true) {
-                            # scale 440 x 275
-                            $image = substr_replace($image, '_440x275', $position, 0);
-                        }
-                    }else{
-                        $image = 'no-image.png';
+            $link = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=' . 330;
+            $path = !$product[DbTable_Product::COL_PRODUCT_FLAG_UPLOAD_IMAGE] ? '' : 'http://' . Application_Constant_Global::DOMAIN . '/upload/';
+            $image = $product[DbTable_Product::COL_PRODUCT_THUMB_NAIL];
+            if (!$path && !$image) {
+                $image = 'no-image.png';
+            } else if ($path) {
+                if ($image) {
+                    $position = strpos($image, '.');
+                    if ($product[DbTable_Product::COL_PRODUCT_FLAG_CROP] == true) {
+                        # scale 440 x 275
+                        $image = substr_replace($image, '_440x275', $position, 0);
                     }
-
+                } else {
+                    $image = 'no-image.png';
                 }
-                $image = $image ? $image : 'no-image.png';
+
+            }
+            $image = $image ? $image : 'no-image.png';
+            $fullPatch = sprintf(
+                '%s&url=%s%s',
+                $link,
+                $path,
+                $image
+            );
+
+            if ($product[DbTable_Product::COL_PRODUCT_FLAG_CROP] == true) {
                 $fullPatch = sprintf(
-                    '%s&url=%s%s',
-                    $link,
+                    '%s%s',
                     $path,
                     $image
                 );
-
-                if ($product[DbTable_Product::COL_PRODUCT_FLAG_CROP] == true) {
-                    $fullPatch = sprintf(
-                        '%s%s',
-                        $path,
-                        $image
-                    );
-                }
-                $image = $fullPatch;
-                $district = isset($dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]]) ? $dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]][DbTable_District::COL_DISTRICT_TYPE] . ' ' . $dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]][DbTable_District::COL_DISTRICT_NAME] : '';
-                $link = '/' . $categoryData[$product[DbTable_Product::COL_PRODUCT_CATEGORY_ID]][DbTable_Product_Category::COL_PRODUCT_CATEGORY_IDENTIFY] . '/ho-chi-minh/' . $dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]][DbTable_District::COL_DISTRICT_IDENTIFY] . '/' . $product[DbTable_Product::COL_PRODUCT_IDENTIFY] . '-' . Model_Product::getInstance()->encode($product[DbTable_Product::COL_PRODUCT_ID]);
-                $price = $product[DbTable_Product::COL_PRODUCT_PAID_PRICE];
-                if($price > 0){
-                    $price =  $product[DbTable_Product::COL_PRODUCT_PAID_PRICE]/1000000 >= 1 ?  ($product[DbTable_Product::COL_PRODUCT_PAID_PRICE]/1000000 .'tr' ): ($product[DbTable_Product::COL_PRODUCT_PAID_PRICE]/1000 .'k');
-                }else{
-                    $price = 'Thỏa thuận';
-                }
-                $productInfo = array(
-                    'latitude' => $product[DbTable_Product::COL_PRODUCT_LAT],
-                    'longitude' => $product[DbTable_Product::COL_PRODUCT_LONG],
-                    'title' => $product[DbTable_Product::COL_PRODUCT_NAME],
-                    "price" => $price,
-                    'area' => $product[DbTable_Product::COL_PRODUCT_AREA],
-                    'phone' => $product[DbTable_Product::COL_PRODUCT_PHONE],
-                    'image' => $image,
-                    'district' => $district,
-                    'link' => $link
-
-                );
-                $productArray[] = $productInfo;
-            }catch (Exception $ex){
+            }
+            $image = $fullPatch;
+            $district = isset($dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]]) ? $dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]][DbTable_District::COL_DISTRICT_TYPE] . ' ' . $dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]][DbTable_District::COL_DISTRICT_NAME] : '';
+            if (!isset($categoryData[$product[DbTable_Product::COL_PRODUCT_CATEGORY_ID]]) || !isset($dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]])) {
                 continue;
             }
+            $link = '/' . $categoryData[$product[DbTable_Product::COL_PRODUCT_CATEGORY_ID]][DbTable_Product_Category::COL_PRODUCT_CATEGORY_IDENTIFY] . '/ho-chi-minh/' . $dataDistrict[$product[DbTable_Product::COL_FK_DISTRICT]][DbTable_District::COL_DISTRICT_IDENTIFY] . '/' . $product[DbTable_Product::COL_PRODUCT_IDENTIFY] . '-' . Model_Product::getInstance()->encode($product[DbTable_Product::COL_PRODUCT_ID]);
+            $price = $product[DbTable_Product::COL_PRODUCT_PAID_PRICE];
+            if ($price > 0) {
+                $price = $product[DbTable_Product::COL_PRODUCT_PAID_PRICE] / 1000000 >= 1 ? ($product[DbTable_Product::COL_PRODUCT_PAID_PRICE] / 1000000 . 'tr') : ($product[DbTable_Product::COL_PRODUCT_PAID_PRICE] / 1000 . 'k');
+            } else {
+                $price = 'Thỏa thuận';
+            }
+            $productInfo = array(
+                'latitude' => $product[DbTable_Product::COL_PRODUCT_LAT],
+                'longitude' => $product[DbTable_Product::COL_PRODUCT_LONG],
+                'title' => $product[DbTable_Product::COL_PRODUCT_NAME],
+                "price" => $price,
+                'area' => $product[DbTable_Product::COL_PRODUCT_AREA],
+                'phone' => $product[DbTable_Product::COL_PRODUCT_PHONE],
+                'image' => $image,
+                'district' => $district,
+                'link' => $link
+
+            );
+            $productArray[] = $productInfo;
 
         }
         return (json_encode($productArray));
