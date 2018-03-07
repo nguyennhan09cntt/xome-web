c12df0c2854c193a43f443f2b70cdd5c5dae299d
Lam Vi Ni <neil@neil.lam>
2018-03-07 22:43:04 +0700

    Log search

---
+++ application/modules/default/models/Dao/LogSearch.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Nhan
+ * Date: 3/19/2016
+ * Time: 5:57 PM
+ */
+class Model_Dao_LogSearch extends DbTable_Log_Search
+{
+    
+}
---
+++ application/modules/default/models/LogSearch.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Nhan
+ * Date: 3/21/2016
+ * Time: 3:59 PM
+ */
+class Model_LogSearch extends Application_Singleton
+{
+
+    /**
+     *
+     * @var Model_Dao_LogSearch
+     */
+    private $_dao;
+
+    protected function __construct()
+    {
+        $this->_dao = new Model_Dao_LogSearch ();
+    }
+
+    public function insert($key_content)
+    {
+        $key_content = trim($key_content);
+        $result = null;
+        try {
+            $params = array(
+                DbTable_Log_Search::COL_KEY_CONTENT => $key_content,
+                DbTable_Log_Search::COL_CREATED_AT => $this->_dao->mysqlSysDate()
+            );
+            $this->_dao->insert($params);            
+        } catch (Exception $e) {
+            $result = $e->getMessage();
+        }
+        return $result;
+    }
+}
